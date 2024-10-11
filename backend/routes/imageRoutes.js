1ac471a730701ff535ea3eb2a829ff4a398dffd6
Kacper Margol <kacpermargol@gmail.com>
2024-10-11 10:25:37 +0200

    Frontend files and routes add

---
+++ backend/routes/imageRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import {
+	getImages,
+	uploadImage,
+	// deleteImage,
+} from '../controllers/imageController.js';
+import { protect } from '../middleware/authMiddleware.js';
+import upload from '../middleware/uploadMiddleware.js';
+
+const router = express.Router();
+
+router.get('/', getImages);
+router.post('/', protect, upload.single('image'), uploadImage);
+// router.delete('/:id', protect, deleteImage);
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import connectDB from './config/db.js';
+
+// Import routes
+import imageRoutes from './routes/imageRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use('/api/images', imageRoutes);
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => {
+	res.send('DS PHOTO is on!');
+});
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
