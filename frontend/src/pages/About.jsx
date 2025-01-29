16a5ed2ee86e94a3f8251538f73d0ba08e93a189
Kacper Margol <kacpermargol@gmail.com>
2025-01-29 14:34:06 +0100

    Fix image URL in About page to remove unnecessary url() wrapper

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -9,7 +9,7 @@ const AboutPage = () => {
 				{/* Image on the left side */}
 				<div className="md:w-1/2 mb-8 md:mb-0">
 					<img
-						src={`url('https://dsphoto.onrender.com/api/assets/Dawid_hero.jpg')`}
+						src="https://dsphoto.onrender.com/assets/Dawid_hero.jpg"
 						alt="Fotograf"
 						className="w-full h-auto rounded-lg shadow-lg"
 					/>
