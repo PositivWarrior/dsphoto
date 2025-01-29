02d19b55819fd5fb2ec6b657490a407e92ad054c
Kacper Margol <kacpermargol@gmail.com>
2025-01-29 15:51:59 +0100

    Fix lazy loading for Contact and Reviews pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,9 @@ const Login = lazy(() =>
 	import('./pages/Login').then((module) => ({ default: module.default })),
 );
 const ContactPage = lazy(() =>
-	import('./pages/Contact').then((module) => ({ default: module.default })),
+	import('./pages/Contact').then((module) => ({
+		default: module.default || module,
+	})),
 );
 const BookTimePage = lazy(() =>
 	import('./pages/BookTime').then((module) => ({ default: module.default })),
@@ -37,7 +39,9 @@ const AboutPage = lazy(() =>
 	import('./pages/About').then((module) => ({ default: module.default })),
 );
 const ReviewsPage = lazy(() =>
-	import('./pages/Reviews').then((module) => ({ default: module.default })),
+	import('./pages/Reviews').then((module) => ({
+		default: module.default || module,
+	})),
 );
 
 function ScrollToTop() {
