518eb239dab0a30dfd6b21189313a09b341210fb
Kacper Margol <kacpermargol@gmail.com>
2025-01-29 15:12:15 +0100

    Add BrowserRouter to enable client-side routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -8,9 +9,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<HelmetProvider>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</HelmetProvider>
-	</React.StrictMode>
+	</React.StrictMode>,
 );
 
 // Defer web vitals reporting
