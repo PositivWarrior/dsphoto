322a672da2f38dbec49b88f268548178c35ce8a0
Kacper Margol <kacpermargol@gmail.com>
2025-01-30 11:58:01 +0100

    Update Admin Panel page title and remove welcome header

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -194,10 +194,8 @@ const Admin = () => {
 	return (
 		<div className="container mx-auto px-4 py-8 mt-20">
 			<Helmet>
-				<title>Admin Panel | Dawid Siedlec</title>
+				<title>Admin Panel | FotoDS</title>
 			</Helmet>
-			<h1 className="text-3xl font-bold text-center mb-8">Admin Panel</h1>
-			<h2 className="text-xl text-center mb-8">Welcome {adminName}</h2>
 
 			<div className="space-y-8">
 				<div className="min-h-screen grid grid-cols-1 md:grid-cols-4">
